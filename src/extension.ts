4537b6b9ebaf70a24a999d3e75700dcca8c5f205
Callum Wright <wrightci01s@gmail.com>
2024-10-19 20:27:29 +0100

    Add description prefix for platform-specific Firefox CSS

--- src/extension.ts
+++ src/extension.ts
@@ -13,27 +13,31 @@ export function activate(context: vscode.ExtensionContext) {
 			for (const [platform, values] of Object.entries(json.completions)) {
 
 				// Skip appropraiate platform-specific completions
+				let descriptionPrefix: string = "";
 				switch (platform) {
 					case "linux":
 						if (!["All", "Linux"].includes(targetPlatform!)) {
 							continue;
 						}
+						descriptionPrefix = "Linux-specific Firefox CSS\n";
 						break;
 					case "osx":
 						if (!["All", "macOS"].includes(targetPlatform!)) {
 							continue;
 						}
+						descriptionPrefix = "macOS-specific Firefox CSS\n";
 						break;
 					case "windows":
 						if (!["All", "Windows"].includes(targetPlatform!)) {
 							continue;
 						}
+						descriptionPrefix = "Windows-specific Firefox CSS\n";
 						break;
 				}
 
 				for (let element of values) {
-					const completion = new vscode.CompletionItem({ label: element.label!, description: "Firefox CSS" }, vscode.CompletionItemKind.Snippet);
-					completion.documentation = new vscode.MarkdownString(`\`\`\`css\n${element.snippet!}`);
+					const completion = new vscode.CompletionItem({ label: element.label!, description: `Firefox CSS` }, vscode.CompletionItemKind.Snippet);
+					completion.documentation = new vscode.MarkdownString(`${descriptionPrefix}\`\`\`css\n${element.snippet!}`);
 					completion.insertText = new vscode.SnippetString(element.snippet!);
 					completions.push(completion);
 				};
