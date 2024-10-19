dfec8ba6d23e51d44506fca1577dbf67b291ce01
Callum Wright <wrightci01s@gmail.com>
2024-10-19 20:37:58 +0100

    Refactor code into functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,40 @@ import json from '../completions.json';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	function isPlatformAllowedByConfiguration(platform: string): boolean {
+		const targetPlatform = vscode.workspace.getConfiguration('firefoxCSS').get<string>('targetPlatform');
+		switch (platform) {
+			case "linux":
+				if (!["All", "Linux"].includes(targetPlatform!)) {
+					return false;
+				}
+			case "osx":
+				if (!["All", "macOS"].includes(targetPlatform!)) {
+					return false;
+				}
+			case "windows":
+				if (!["All", "Windows"].includes(targetPlatform!)) {
+					return false;
+				}
+			default:
+				return true;
+		}
+	}
+
+	function getDesriptionPrefix(platform: string): string {
+		const postfix: string = "-specific Firefox CSS\n";
+		switch (platform) {
+			case "linux":
+				return `Linux${postfix}`;
+			case "osx":
+				return `macOS${postfix}`;
+			case "windows":
+				return `Windows${postfix}`;
+			default:
+				return "";
+		}
+	}
+
 	const completion = vscode.languages.registerCompletionItemProvider({ pattern: '**/userChrome.css' }, {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
 
@@ -12,32 +46,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 			for (const [platform, values] of Object.entries(json.completions)) {
 
-				// Skip appropraiate platform-specific completions
-				let descriptionPrefix: string = "";
-				switch (platform) {
-					case "linux":
-						if (!["All", "Linux"].includes(targetPlatform!)) {
-							continue;
-						}
-						descriptionPrefix = "Linux-specific Firefox CSS\n";
-						break;
-					case "osx":
-						if (!["All", "macOS"].includes(targetPlatform!)) {
-							continue;
-						}
-						descriptionPrefix = "macOS-specific Firefox CSS\n";
-						break;
-					case "windows":
-						if (!["All", "Windows"].includes(targetPlatform!)) {
-							continue;
-						}
-						descriptionPrefix = "Windows-specific Firefox CSS\n";
-						break;
+				if (!isPlatformAllowedByConfiguration(platform)) {
+					continue;
 				}
 
 				for (let element of values) {
 					const completion = new vscode.CompletionItem({ label: element.label!, description: `Firefox CSS` }, vscode.CompletionItemKind.Snippet);
-					completion.documentation = new vscode.MarkdownString(`${descriptionPrefix}\`\`\`css\n${element.snippet!}`);
+					completion.documentation = new vscode.MarkdownString(`${getDesriptionPrefix(platform)}\`\`\`css\n${element.snippet!}`);
 					completion.insertText = new vscode.SnippetString(element.snippet!);
 					completions.push(completion);
 				};
