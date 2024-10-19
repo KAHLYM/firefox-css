ce0fb6caa33aeab4e421fe2b548f16a03076bdf0
Callum Wright <wrightci01s@gmail.com>
2024-10-19 21:58:53 +0100

    Add platform specific completions (#6)
    PLatforms include linux, macOS and Windows with shared completions for
    all.
    Default is all platforms.
    
    ### Added
    * Platform specific completions
    * Configuration to target specific platform completions
    
    Tracked against: None

---
+++ src/extension.test.ts
@@ -0,0 +1,45 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import * as myExtension from './extension';
+
+suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+
+    test('Sample test', () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+
+    [
+        { platform: "linux", targetPlatform: "Linux", allowed: true },
+        { platform: "linux", targetPlatform: "All", allowed: true },
+        { platform: "linux", targetPlatform: "non-platform", allowed: false },
+        { platform: "osx", targetPlatform: "macOS", allowed: true },
+        { platform: "osx", targetPlatform: "All", allowed: true },
+        { platform: "osx", targetPlatform: "non-platform", allowed: false },
+        { platform: "windows", targetPlatform: "Windows", allowed: true },
+        { platform: "windows", targetPlatform: "All", allowed: true },
+        { platform: "windows", targetPlatform: "non-platform", allowed: false },
+        { platform: "shared", targetPlatform: "All", allowed: true },
+        { platform: "shared", targetPlatform: "non-platform", allowed: true },
+        { platform: "", targetPlatform: "", allowed: true }, // Default
+    ].forEach(function (item) {
+        test("isPlatformAllowedByConfiguration return for platform'" + item.platform + " with target platform '" + item.targetPlatform + "'", () => {
+            assert.equal(item.allowed, myExtension.isPlatformAllowedByConfiguration(item.platform, item.targetPlatform));
+        });
+    });
+
+    [
+        { platform: "linux", descriptionPrefix: "Linux-specific Firefox CSS" },
+        { platform: "osx", descriptionPrefix: "macOS-specific Firefox CSS" },
+        { platform: "windows", descriptionPrefix: "Windows-specific Firefox CSS" },
+        { platform: "non-platform", descriptionPrefix: "" },
+    ].forEach(function (item) {
+        test("getDesriptionPrefix return for '" + item.platform + "'", () => {
+            assert.equal(item.descriptionPrefix, myExtension.getDesriptionPrefix(item.platform));
+        });
+    });
+});
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,41 @@
 import * as vscode from 'vscode';
 import json from '../completions.json';
 
+export function isPlatformAllowedByConfiguration(platform: string, targetPlatform_: string = ""): boolean {
+	const targetPlatform = targetPlatform_ ? targetPlatform_ : vscode.workspace.getConfiguration('firefoxCSS').get<string>('targetPlatform');
+	switch (platform) {
+		case "linux":
+			if (!["All", "Linux"].includes(targetPlatform!)) {
+				return false;
+			}
+		case "osx":
+			if (!["All", "macOS"].includes(targetPlatform!)) {
+				return false;
+			}
+		case "windows":
+			if (!["All", "Windows"].includes(targetPlatform!)) {
+				return false;
+			}
+		default:
+			return true;
+	}
+}
+
+export function getDesriptionPrefix(platform: string): string {
+	const postfix: string = "-specific Firefox CSS\n";
+	switch (platform) {
+		case "linux":
+			return `Linux${postfix}`;
+		case "osx":
+			return `macOS${postfix}`;
+		case "windows":
+			return `Windows${postfix}`;
+		default:
+			return "";
+	}
+}
+
+/* istanbul ignore next: Difficult to unit test */
 export function activate(context: vscode.ExtensionContext) {
 
 	const completion = vscode.languages.registerCompletionItemProvider({ pattern: '**/userChrome.css' }, {
@@ -8,11 +43,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 			let completions: vscode.CompletionItem[] = [];
 
-			for (let element of json.completions) {
-				const completion = new vscode.CompletionItem({ label: element.label!, description: "Firefox CSS" }, vscode.CompletionItemKind.Snippet);
-				completion.documentation = new vscode.MarkdownString(`\`\`\`css\n${element.snippet!}`);
-				completion.insertText = new vscode.SnippetString(element.snippet!);
-				completions.push(completion);
+			for (const [platform, values] of Object.entries(json.completions)) {
+
+				if (!isPlatformAllowedByConfiguration(platform)) {
+					continue;
+				}
+
+				// @ts-ignore: TS2488
+				for (const element of values) {
+					const completion = new vscode.CompletionItem({ label: element.label!, description: `Firefox CSS` }, vscode.CompletionItemKind.Snippet);
+					completion.documentation = new vscode.MarkdownString(`${getDesriptionPrefix(platform)}\`\`\`css\n${element.snippet!}`);
+					completion.insertText = new vscode.SnippetString(element.snippet!);
+					completions.push(completion);
+				};
 			};
 
 			return completions;
@@ -22,4 +65,5 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(completion);
 }
 
+/* istanbul ignore next: Empty function */
 export function deactivate() { }
