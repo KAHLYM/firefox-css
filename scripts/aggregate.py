4762ef03d8417ff0fbee51b9bdba218b87c84e39
Callum Wright <wrightci01s@gmail.com>
2024-10-22 18:15:04 +0100

    Support all AST node classes (#11)
    ### Changed
    * Evaluation of rules to support all AST node classes including comments
    within rules
    
    Tracked against: None

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -15,35 +15,43 @@ parser.add_argument(
 args = parser.parse_args()
 
 
+def expand_values(components) -> str:
+    value = ""
+
+    for component in components:
+
+        if isinstance(component, tinycss2.ast.Comment):
+            value += f"/*{component.value}*/"
+        elif isinstance(component, tinycss2.ast.CurlyBracketsBlock):
+            value += f"{{{expand_values(component.content)}}}"
+        elif isinstance(component, tinycss2.ast.FunctionBlock):
+            value += f"{component.name}({expand_values(component.arguments)})"
+        elif isinstance(component, tinycss2.ast.SquareBracketsBlock):
+            value += f"[{expand_values(component.content)}]"
+        elif isinstance(component, tinycss2.ast.ParenthesesBlock):
+            value += f"({expand_values(component.content)})"
+        else:
+            value += str(component.value)
+
+    return value
+
+
 def get_completions(source: str):
     completions = []
     for file in glob.glob(f"{args.path}/{source}/**/*.css"):
         with open(file) as f:
             rules = tinycss2.parse_stylesheet(
-                f.read(), skip_comments=True, skip_whitespace=True
+                f.read(), skip_whitespace=True
             )
 
             for rule in rules:
 
-                if (
-                    rule.prelude is None
-                    or rule.content is None
-                    or any(
-                        isinstance(
-                            element,
-                            (
-                                tinycss2.ast.CurlyBracketsBlock,
-                                tinycss2.ast.FunctionBlock,
-                                tinycss2.ast.SquareBracketsBlock,
-                            ),
-                        )
-                        for element in (rule.prelude + rule.content)
-                    )
-                ):
+                # No interest in comments that are not within rule
+                if isinstance(rule, tinycss2.ast.Comment):
                     continue
 
-                prelude = "".join([element.value for element in rule.prelude])
-                content = "".join([str(element.value) for element in rule.content])
+                prelude = expand_values(rule.prelude) if rule.prelude else ""
+                content = expand_values(rule.content) if rule.content else ""
 
                 completions.append(
                     {"label": prelude, "snippet": f"{prelude}{{{content}}}"}
