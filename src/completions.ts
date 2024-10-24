160024ff33e95c1ba3d41669621ff246fbe50aa6
Callum Wright <wrightci01s@gmail.com>
2024-10-24 19:56:55 +0100

    Add configuration

--- src/completions.ts
+++ src/completions.ts
@@ -1,6 +1,6 @@
 export let completions: any;
-export async function downloadCompletions(): Promise<string | void> {
-    fetch("https://raw.githubusercontent.com/KAHLYM/firefox-css/refs/heads/main/completions.json")
+export async function downloadCompletions(source: string): Promise<string | void> {
+    fetch(`https://raw.githubusercontent.com/KAHLYM/firefox-css/refs/heads/main/completions/${source}.json`)
         .then(res => res.text())
         .then(text => JSON.parse(text))
         .then(json => {
