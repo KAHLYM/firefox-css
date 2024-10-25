de4e8cfcade93aaa30af4c60b88ff7ff3bde604a
Callum Wright <wrightci01s@gmail.com>
2024-10-25 18:21:37 +0100

    Update url for completions

--- src/completions.ts
+++ src/completions.ts
@@ -1,6 +1,6 @@
 export let completions: any;
 export async function downloadCompletions(source: string): Promise<string | void> {
-    fetch(`https://raw.githubusercontent.com/KAHLYM/firefox-css/refs/heads/main/completions/${source}.json`)
+    fetch(`https://raw.githubusercontent.com/KAHLYM/firefox-css/refs/heads/completions/completions/${source}.json`)
         .then(res => res.text())
         .then(text => JSON.parse(text))
         .then(json => {
