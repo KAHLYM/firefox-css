49b6937b462f4f52f90bfce21e3494dc779967a9
Callum Wright <wrightci01s@gmail.com>
2024-11-09 19:42:07 +0000

    Get completions from cache

--- src/completions.ts
+++ src/completions.ts
@@ -1,18 +1,20 @@
 import * as vscode from 'vscode';
 import { output } from './extension';
-var constants = require('./constants');
+const constants = require('./constants');
+const path = require('path');
 
 export async function getCompletions(source: string): Promise<any> {
-    let completions = await download(source);
+    // This should be a JSON object
+    let completions: any = await download(source);
 
     if (!completions) { // then attempt to get completions from cache
-        vscode.window.showWarningMessage("Failed to download completions.",
+        await vscode.window.showWarningMessage("Failed to download completions.",
             constants.strings.message.getCompletions.TRY_AGAIN, constants.strings.message.getCompletions.USE_CACHE)
             .then(async value => {
                 if (value == constants.strings.message.getCompletions.TRY_AGAIN) {
                     completions = await getCompletions(source);
                 } else if (value == constants.strings.message.getCompletions.USE_CACHE) {
-                    completions = getFromCache();
+                    completions = await getCache(source);
                 }
             });
     }
@@ -28,6 +30,9 @@ async function download(source: string): Promise<any> {
         const json = JSON.parse(text);
 
         output.appendLine(`Downloaded completions (${text.length} bytes) from: ${url}`);
+
+        setCache(source, text);
+
         return json;
 
     } catch (error) {
@@ -36,6 +41,34 @@ async function download(source: string): Promise<any> {
     }
 }
 
-function getFromCache(): void {
+function getCacheUri(source: string) : vscode.Uri {
+    return vscode.Uri.file(`${process.env.LOCALAPPDATA!}${path.sep}${constants.extension.NAME}${path.sep}${source}.json`);
+}
 
+async function getCache(source: string): Promise<any> {
+    try {
+        const uri: vscode.Uri = getCacheUri(source);
+        return await vscode.workspace.fs.readFile(uri).then(async content => {
+            const decoder = new TextDecoder("utf-8");
+            const decoded = decoder.decode(content);
+            const json = JSON.parse(decoded);
+
+            output.appendLine(`Fetched completions (${content.length} bytes) from: ${uri.toString()}`);
+
+            return json;
+        });
+    } catch (error) {
+        /* istanbul ignore next: difficult to force readFile to throw */
+        console.error("Failed to read completions from cache: ", error);
+    }
+}
+
+function setCache(source: string, content: string): void {
+    try {
+        const encoder = new TextEncoder();
+        vscode.workspace.fs.writeFile(getCacheUri(source), encoder.encode(content));
+    } catch (error) {
+        /* istanbul ignore next: difficult to force writeFile to throw */
+        console.error("Failed to write completions to cache: ", error);
+    }
 }
